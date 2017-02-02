01d83429e3c23a1b3602cf5f373712d701881bab
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-02 08:11:14 +0530

    Using just 3 parameters in the first iteration

--- osmcha/changeset.py
+++ osmcha/changeset.py
@@ -296,8 +296,6 @@ class Analyse(object):
 
         def changeset_to_data(changeset):
             return [
-                len(changeset.editor),
-                len(changeset.source),
                 changeset.create,
                 changeset.modify,
                 changeset.delete
