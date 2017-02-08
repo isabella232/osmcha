35b75c8fe3d614729ac167801368e89624ba07ac
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-08 14:23:39 +0530

    Updated "Flagged by ML classifier" to "Flagged by gabbar"

--- osmcha/changeset.py
+++ osmcha/changeset.py
@@ -221,7 +221,7 @@ class Analyse(object):
 
     def prediction_from_gabbar(self):
 
-        reason = 'Flagged by ML classifier'
+        reason = 'Flagged by gabbar'
         model = gabbar.load_model()
 
         # Passing a dictionary converted from a Django object.
