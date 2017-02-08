bd9d7bb49226b6abbf37b3f2d05f0cc3c3ef01c7
Bhargav Kowshik <bhargav@mapbox.com>
2017-02-08 14:23:39 +0530

    Updated "Flagged by ML classifier" to "Flagged by gabbar"

--- tests/test_mod.py
+++ tests/test_mod.py
@@ -512,5 +512,5 @@ def test_get_dict():
 def test_prediction_from_gabbar():
     changeset = Analyse(31450443)
     changeset.full_analysis()
-    assert 'Flagged by ML classifier' in changeset.suspicion_reasons
+    assert 'Flagged by gabbar' in changeset.suspicion_reasons
     assert changeset.is_suspect
